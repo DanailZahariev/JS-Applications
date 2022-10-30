30e0e8e7e83eeec61422f7778060fc123b27da5a
danail <d.zahariev@icloud.com>
2022-10-30 18:37:22 +0200

    Asynchronous Programming Lab

--- Asynchronous Programming/lab/02.Github-Repos/scripts/app.js
+++ Asynchronous Programming/lab/02.Github-Repos/scripts/app.js
@@ -1,3 +1,23 @@
-function loadRepos() {
-	
+async function loadRepos() {
+
+    let username = document.getElementById('username').value;
+    let repos = document.getElementById('repos');
+
+    try {
+        const response = await fetch(`https://api.github.com/users/${username}/repos`);
+
+        if (!response.ok) {
+            throw new Error(`${response.status} ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        repos.innerHTML = '';
+
+        for (let repo of data) {
+            repos.innerHTML += `<li><a href="${repo.html_url}">${repo.full_name}</a></li>`
+        }
+
+    } catch (e) {
+        repos.innerHTML = `${e.message}`;
+    }
 }
--- Asynchronous Programming/lab/03.Gighub-Commits/app.js
+++ Asynchronous Programming/lab/03.Gighub-Commits/app.js
@@ -1,4 +1,27 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+
+    let username = document.getElementById('username').value;
+    let userRepo = document.getElementById('repo').value;
+    let commits = document.getElementById('commits');
+
+    try {
+        const response = await fetch(`https://api.github.com/repos/${username}/${userRepo}/commits`);
+
+        if (!response.ok) {
+            throw new Error(`${response.status} ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        const items = data.map(data => {
+            const li = document.createElement('li');
+            li.textContent = `${data.commit.author.name}: ${data.commit.message}`;
+
+            return li;
+        });
+
+        commits.replaceChildren(...items);
+
+    } catch (e) {
+        commits.textContent = e.message;
+    }
 }
