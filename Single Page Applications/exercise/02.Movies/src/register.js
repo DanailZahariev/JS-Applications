593cee062f806c4986f311ecf0c65ba31b0fe161
danail <d.zahariev@icloud.com>
2022-11-15 15:49:16 +0200

    SPA Exercise - movies in progress

--- Single Page Applications/exercise/02.Movies/src/register.js
+++ Single Page Applications/exercise/02.Movies/src/register.js
@@ -1,7 +1,45 @@
-import {showView} from "./util.js";
+import {showView, updateNav} from "./util.js";
+import {homePage} from "./home.js";
 
 const registerSection = document.querySelector('#form-sign-up');
+const form = registerSection.querySelector('form');
+form.addEventListener('submit', registerUser);
 
 export function registerPage() {
     showView(registerSection);
+}
+
+async function registerUser(evt) {
+    evt.preventDefault();
+
+    const formData = new FormData(form);
+    const email = formData.get('email');
+    const password = formData.get("password");
+    const confirmPassword = formData.get("repeatPassword");
+
+    if (!email || !password || !confirmPassword || password !== confirmPassword) {
+        alert('All fields are required');
+    }
+
+    try {
+        const response = await fetch('http://localhost:3030/users/register', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({email, password})
+        });
+
+        if (!response.ok) {
+            const error = await response.json();
+            throw new Error(error.message);
+        }
+        const user = await response.json();
+        localStorage.setItem('user', JSON.stringify(user));
+
+        form.reset();
+        updateNav();
+        await homePage();
+
+    } catch (e) {
+        alert(e.message);
+    }
 }
