380d62c17dc63e6149e56b877c88d5544e7abcdd
danail <d.zahariev@icloud.com>
2022-11-20 22:31:50 +0200

    Client Side Rendering - lab

---
+++ Client Side Rendering/lab/01.Contacts/app.js
@@ -0,0 +1,40 @@
+import { html, nothing, render } from "/node_modules/lit-html/lit-html.js";
+import {contacts as data} from "./contacts.js";
+
+const root = document.getElementById('contacts');
+const contacts = data.map(c => Object.assign({}, c, {active: false}));
+
+const contactCard = (contact) =>
+    html`
+        <div class="contact card">
+            <div>
+                <i class="far fa-user-circle gravatar"></i>
+            </div>
+            <div class="info">
+                <h2>Name: ${contact.name}</h2>
+                <button id=${contact.id} class="detailsBtn">Details</button>
+                ${contact.active
+                        ? html`
+                            <div class="details">
+                                <p>Phone number: ${contact.phoneNumber}</p>
+                                <p>Email: ${contact.email}</p>
+                            </div>`
+                        : nothing}
+            </div>
+        </div>`;
+
+root.addEventListener('click', onToggle);
+update();
+
+function update() {
+    render(contacts.map(contactCard), root);
+}
+
+function onToggle(event) {
+    if (event.target.classList.contains('detailsBtn')) {
+        const id = event.target.id;
+        const contact = contacts.find(c => c.id == id);
+        contact.active = !contact.active;
+        update();
+    }
+}
